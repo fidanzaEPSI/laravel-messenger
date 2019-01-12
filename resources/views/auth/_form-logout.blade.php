89f8e2b72899d5acb0890c1926dd6dd06f84315f
fidanzaEPSI <fidanza.frank@gmail.com>
2019-01-12 16:05:20 +0100

    Fixed language on form logout

--- resources/views/auth/_form-logout.blade.php
+++ resources/views/auth/_form-logout.blade.php
@@ -1,7 +1,7 @@
 <a class="dropdown-item" href="{{ route('logout') }}"
     onclick="event.preventDefault();
                 document.getElementById('logout-form').submit();">
-    Déconnexion
+    Logout
 </a>
 
 <form id="logout-form" action="{{ route('logout') }}" method="POST" hidden>
